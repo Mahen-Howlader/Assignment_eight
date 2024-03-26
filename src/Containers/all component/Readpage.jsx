ae396a038a05e54ecc6b2ba65107b2f4ba538a7e
devmahin <mahinhowlader123@gmail.com>
2024-03-26 23:13:52 +0600

    The listed books inside read books and wishbooks completed

---
+++ src/Containers/all component/Readpage.jsx
@@ -0,0 +1,84 @@
+import { IoLocationOutline } from "react-icons/io5";
+import { GoPeople } from "react-icons/go";
+import { MdOutlineContactPage } from "react-icons/md";
+import { Link } from "react-router-dom";
+function Readpage({ data }) {
+  //   console.log(data);
+  const {
+    bookId,
+    bookName,
+    author,
+    image,
+    review,
+    totalPages,
+    rating,
+    category,
+    tags,
+    publisher,
+    yearOfPublishing,
+  } = data || {};
+
+  return (
+    <div className="my-10">
+      <div className="flex border-4 rounded-xl flex-col  p-6 space-y-4 sm:p-10 dark:bg-gray-50 dark:text-gray-800">
+        <ul className="flex flex-col divide-y dark:divide-gray-300">
+          <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
+            <div className="flex w-full space-x-2 sm:space-x-4">
+              <div className="w-96 bg-base-300 flex justify-center rounded-xl  p-5">
+                <img
+                  className="flex-shrink-0 h-full object-contain  dark:border- rounded outline-none dark:bg-gray-500"
+                  src={image}
+                  alt="Polaroid camera"
+                />
+              </div>
+              <div className="flex flex-col justify-between w-full pb-4">
+                <div className="space-y-5">
+                  <h3 className="text-4xl font-semibold leading-snug sm:pr-8">
+                    {bookName}
+                  </h3>
+                  <p className="text-lg font-semibold">By: {author}</p>
+                  <div className="flex gap-x-10">
+                    <div className="flex gap-x-4 items-center text-xl">
+                      <span className="font-bold">Tag :</span>
+                      <ul className="flex  gap-x-5">
+                        {tags.map((val, index) => (
+                          <li
+                            className="bg-[#F3F3F3] p-1 rounded-lg font-semibold text-[#23BE0A]"
+                            key={index}
+                          >
+                            #{val}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className="flex items-center gap-1">
+                      <IoLocationOutline size={20} />
+                      <h2>Year of publishing : {yearOfPublishing}</h2>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-x-10 text-xl">
+                    <h2 className="flex items-center gap-x-2">
+                      <GoPeople /> <span>Publisher : {publisher}</span>
+                    </h2>
+                    <h2 className="flex items-center gap-x-2">
+                      <MdOutlineContactPage />
+                      <span>page : {totalPages}</span>
+                    </h2>
+                  </div>
+                  {/* btn  */}
+                  <div className="flex gap-x-8">
+                    <button className="bg-[#E0EEFF] font-semibold py-2 px-3 rounded-lg text-lg">Category : {category}</button>
+                    <button className="bg-[#FFF3E0] font-semibold py-2 px-3 rounded-lg text-lg">Rating : {rating}</button>
+                    <Link to={`/bookdetails/${bookId}`} className="bg-[#23BE0A] font-semibold text-white py-2 px-3 rounded-lg text-lg">View Details</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Readpage;
--- src/Page/ListesBook.jsx
+++ src/Page/ListesBook.jsx
@@ -1,21 +1,74 @@
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { getLocalStoresGetData } from "../utils/utilitis";
+import Readpage from "../Containers/all component/Readpage";
+import { useEffect, useState } from "react";
 function ListesBook() {
+  const [getRead, setgetRead] = useState([]);
+  const [getWishList, setgetWishList] = useState([]);
+  useEffect(() => {
+    const getRead = getLocalStoresGetData("read");
+    setgetRead(getRead);
+    const getWishList = getLocalStoresGetData("wishlist");
+    setgetWishList(getWishList);
+  }, []);
+
   return (
     <div>
       <div className="p-10 text-3xl font-bold text-center rounded-lg bg-base-200">
         <h1>Books</h1>
       </div>
       <div className="flex justify-center my-2">
-      <details className="dropdown">
-        <summary className="m-1 btn bg-[#23BE0A]  text-white ">open or close</summary>
-        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-300 rounded-box w-36">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
-        </ul>
-      </details>
+        <details className="dropdown">
+          <summary className="m-1 btn bg-[#23BE0A]  text-white ">
+            open or close
+          </summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-300 rounded-box w-36">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </details>
+      </div>
+      <div>
+        <Tabs>
+          <TabList>
+            <Tab>Read Books</Tab>
+            <Tab>Wishlist Books</Tab>
+          </TabList>
+
+          <TabPanel>
+            {getRead.map((val) => (
+              <Readpage key={val.bookId} data={val}></Readpage>
+            ))}
+            <div className="mb-10">
+              <div className="flex justify-center space-x-4">
+                <button
+                  type="button"
+                  className="px-6 py-2 border rounded-md dark:border-violet-600"
+                >
+                  Back
+                  <span className="sr-only sm:not-sr-only">to shop</span>
+                </button>
+                <button
+                  type="button"
+                  className="px-6 py-2 border rounded-md dark:bg-violet-600 dark:text-gray-50 dark:border-violet-600"
+                >
+                  <span className="sr-only sm:not-sr-only">Continue to</span>
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+          {getWishList.map((val) => (
+              <Readpage key={val.bookId} data={val}></Readpage>
+            ))}
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
